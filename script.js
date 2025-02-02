0b405cb3b4c11dd34ac0a107f6bfd717f09b7356
Shobhit9670 <mailofshobhit2005@gmail.com>
2025-02-02 13:14:39 +0530

    Add files via upload
    ok

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    var moreInfo = document.getElementById('moreInfo');
+    if (moreInfo.style.display === 'none') {
+      moreInfo.style.display = 'block';
+    } else {
+      moreInfo.style.display = 'none';
+    }
+  });
